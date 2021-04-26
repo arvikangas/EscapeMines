eb4a8ed5b2a4403df359494c530e4d06ab476453
Arvi Kangas <arvi.kangas@gmail.com>
2021-04-26 21:01:43 +0300

    input parser. part 2

--- EscapeMines.Tests/InputParserTests.cs
+++ EscapeMines.Tests/InputParserTests.cs
@@ -1,3 +1,4 @@
+using EscapeMines.Exceptions;
 using EscapeMines.Models;
 using EscapeMines.Services;
 using Shouldly;
@@ -10,7 +11,7 @@ namespace EscapeMines.Tests
     public class InputParserTests
     {
         [Fact]
-        public void Test1()
+        public void parser_should_return_correct_data_fixture1()
         {
             var inputString = @"5 4
 1,1 1,3 3,3
@@ -23,7 +24,7 @@ R M M M";
             result.ShouldNotBeNull();
 
             result.BoardSize.X.ShouldBe(5);
-            result.BoardSize.X.ShouldBe(4);
+            result.BoardSize.Y.ShouldBe(4);
 
             result.Mines.ShouldContain(new Coord(1, 1));
             result.Mines.ShouldContain(new Coord(1, 3));
@@ -46,6 +47,74 @@ R M M M";
             movesList[7].ShouldBe(Move.Move);
             movesList[8].ShouldBe(Move.Move);
         }
+
+        [Fact]
+        public void parser_should_return_correct_data_fixture2()
+        {
+            var inputString = @"100 23456
+1,1 1,3 3,3 6,6 7,7 8,8 9,9 10,10
+98 78
+0 1 S
+R M L M M M M M M M M M M M";
+            var result = _inputParser.Parse(inputString);
+
+            result.ShouldNotBeNull();
+
+            result.BoardSize.X.ShouldBe(100);
+            result.BoardSize.Y.ShouldBe(23456);
+
+            result.Mines.ShouldContain(new Coord(1, 1));
+            result.Mines.ShouldContain(new Coord(1, 3));
+            result.Mines.ShouldContain(new Coord(3, 3));
+            result.Mines.ShouldContain(new Coord(6, 6));
+            result.Mines.ShouldContain(new Coord(7, 7));
+            result.Mines.ShouldContain(new Coord(8, 8));
+            result.Mines.ShouldContain(new Coord(9, 9));
+            result.Mines.ShouldContain(new Coord(10, 10));
+
+            result.Exit.ShouldBe(new Coord(98, 78));
+
+            result.Turtle.Coord.ShouldBe(new Coord(0, 1));
+            result.Turtle.Direction.ShouldBe(Direction.South);
+
+            var movesList = result.Moves.ToList();
+            movesList.Count.ShouldBe(14);
+            movesList[0].ShouldBe(Move.Right);
+            movesList[1].ShouldBe(Move.Move);
+            movesList[2].ShouldBe(Move.Left);
+            movesList[3].ShouldBe(Move.Move);
+            movesList[4].ShouldBe(Move.Move);
+            movesList[5].ShouldBe(Move.Move);
+            movesList[6].ShouldBe(Move.Move);
+            movesList[7].ShouldBe(Move.Move);
+            movesList[9].ShouldBe(Move.Move);
+            movesList[10].ShouldBe(Move.Move);
+            movesList[11].ShouldBe(Move.Move);
+            movesList[12].ShouldBe(Move.Move);
+            movesList[13].ShouldBe(Move.Move);
+        }
+
+        [Fact]
+        public void parser_should_throw_exception_with_invalid_line_count()
+        {
+            var inputString = @"100 23456
+1,1 1,3 3,3 6,6 7,7 8,8 9,9 10,10
+98 78";
+            Should.Throw<InvalidInputStringException>(() => _inputParser.Parse(inputString));
+        }
+
+        [Fact]
+        public void parser_should_throw_exception_with_direction_symbol()
+        {
+            var inputString = @"5 4
+1,1 1,3 3,3
+4 2
+0 1 N
+R M L X M
+R M M M";
+            Should.Throw<InvalidInputStringException>(() => _inputParser.Parse(inputString));
+        }
+
 
         private readonly IInputParser _inputParser;
 
--- src/EscapeMines/Services/InputParser.cs
+++ src/EscapeMines/Services/InputParser.cs
@@ -20,7 +20,7 @@ namespace EscapeMines.Services
             var boardSize = lines[0].Split(' ');
             if(boardSize.Length != 2)
             {
-                throw new InvalidInputStringException($"Board size variable count {boardSize.Length}. Expected 2");
+                throw new InvalidInputStringException($"Board size data count {boardSize.Length}. Expected 2");
             }
             result.BoardSize = new Coord(int.Parse(boardSize[0]), int.Parse(boardSize[1]));
 
@@ -31,12 +31,63 @@ namespace EscapeMines.Services
                 var mineCoords = mine.Split(',');
                 if(mineCoords.Length != 2)
                 {
-                    throw new InvalidInputStringException($"Mine variable count {boardSize.Length}. Expected 2");
+                    throw new InvalidInputStringException($"Mine data count {boardSize.Length}. Expected 2");
                 }
-                minesList.Add(new Coord(int.Parse(mineCoords[0]), int.Parse(mineCoords[1]));
+                minesList.Add(new Coord(int.Parse(mineCoords[0]), int.Parse(mineCoords[1])));
             }
+            result.Mines = minesList;
 
-            return input;
+            var exit = lines[2].Split(' ');
+            if (boardSize.Length != 2)
+            {
+                throw new InvalidInputStringException($"Exit data count {boardSize.Length}. Expected 2");
+            }
+            result.Exit = new Coord(int.Parse(exit[0]), int.Parse(exit[1]));
+
+            result.Turtle = new Turtle();
+            var turle = lines[3].Split(' ');
+            if (turle.Length != 3)
+            {
+                throw new InvalidInputStringException($"Exit variable count {boardSize.Length}. Expected 3");
+            }
+            result.Turtle.Coord = new Coord(int.Parse(turle[0]), int.Parse(turle[1]));
+            result.Turtle.Direction = GetDirection(turle[2]);
+
+            var moves = new List<Move>();
+            for (int i = 4; i < lines.Length; i++)
+            {
+                var line = lines[i].Split(" ");
+                foreach (var item in line)
+                {
+                    moves.Add(GetMove(item));
+                }
+            }
+            result.Moves = moves;
+
+            return result;
+        }
+
+        private Direction GetDirection(string s)
+        {
+            switch(s)
+            {
+                case "N": return Direction.North; 
+                case "S": return Direction.South;
+                case "E": return Direction.East; 
+                case "W": return Direction.West;
+                default: throw new InvalidInputStringException($"Cannot parse string {s} to Direction");
+            }
+        }
+
+        private Move GetMove(string s)
+        {
+            switch (s)
+            {
+                case "R": return Move.Right;
+                case "L": return Move.Left;
+                case "M": return Move.Move;
+                default: throw new InvalidInputStringException($"Cannot parse string {s} to Move");
+            }
         }
     }
 }
